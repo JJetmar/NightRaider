dfeba8ec7fa4cd7c9c0d4aa733a44dfb379ad674
Josef Jetmar <Jetmar.osobni@gmail.com>
2020-02-13 22:35:01 +0100

    added synchronizer in development branch

---
+++ sync.js
@@ -0,0 +1,8 @@
+const sf = require('sync-folders');
+sf(
+    './NightRaider',
+    'C:\\Program Files (x86)\\World of Warcraft\\_classic_\\Interface\\AddOns\\NightRaider\\',
+    {
+        watch: true
+    }
+);
